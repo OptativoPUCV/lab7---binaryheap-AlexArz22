1b84f73cb9b82b0211d9b0a2fe5efdc1eae240e9
Replit user <>
2023-05-11 00:00:40 +0000

    new try

--- heap.c
+++ heap.c
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <ctype.h>
 #include "heap.h"
+#include <stdbool.h>
 
 typedef struct nodo{
    void* data;
@@ -24,10 +25,30 @@ void* heap_top(Heap* pq){
     
 }
 
-
-
 void heap_push(Heap* pq, void* data, int priority){
-
+  if(pq->size==pq->capac){
+    pq->heapArray=realloc(pq->heapArray, sizeof(heapElem)*pq->capac*2);
+  }
+  heapElem par;
+  par.data=data;
+  par.priority=priority;
+  pq->heapArray[pq->size]=par;
+  int posicion;
+  int padre=(posicion-1)/2;
+  pq->size++;
+  
+  while(padre>0){
+    if(pq->heapArray[posicion].priority>=pq->heapArray[padre].priority){
+      heapElem aux=pq->heapArray[posicion];
+      pq->heapArray[posicion]=pq->heapArray[padre];
+      pq->heapArray[padre]=aux;
+      padre= (posicion-1)/2;
+      posicion = (posicion-1)/2;
+    }
+    else{
+      break;
+    }
+  }
 }
 
 
